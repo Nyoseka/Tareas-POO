a030967759c1f9e817c00d1715cb630c4b98fd1c
Nyoseka <47579115+Nyoseka@users.noreply.github.com>
2019-04-01 14:20:14 -0800

    Update Program.cs

--- TareaUnidad3/Dibujo/Program.cs
+++ TareaUnidad3/Dibujo/Program.cs
@@ -1,7 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Dibujo
 {
+    class Figura
+    {
+        public string color;
+        public int poX;
+        public int poY;
+        public Figura()
+        {
+            color=" ";
+            poX=10;
+            poY=10;
+        }
+        public Figura(string color, int poX, int poY)
+        {
+            this.color=color;
+            this.poX=poX;
+            this.poY;
+        }
+    }
     class Program
     {
         static void Main(string[] args)

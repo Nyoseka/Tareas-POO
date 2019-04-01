9f6af6b6b6b18e3260cc0f2b96729b339a82accd
Nyoseka <47579115+Nyoseka@users.noreply.github.com>
2019-04-01 14:46:34 -0800

    Update Program.cs

--- TareaUnidad3/Dibujo/Program.cs
+++ TareaUnidad3/Dibujo/Program.cs
@@ -18,27 +18,80 @@ namespace Dibujo
         {
             this.color=color;
             this.poX=poX;
-            this.poY;
+            this.poY=poY;
         }
         public virtual void Dibuja()
         {
-            Console.WriteLine("Figura de color {0} en la posición: ({1},{2})",color,poX,poY);
+            Console.WriteLine("Se dibujó una figura de color {0} en la posición: ({1},{2})",color,poX,poY);
         }
     }
     class Rectangulo:Figura
     {
-        string alto;
-        string ancho;
         public Rectangulo(string color, int poX, int poY):base(color,poX,poY)
         {
-            
+            color="blanco";
+            poX=10;
+            poY=10;
+        }
+        public override void Dibuja()
+        {
+            Console.WriteLine("Se dibujó un rectangulo de color {0} en la posición ({1},{2})", color, poX,poY);
+        }
+    }
+    class Circulo:Figura
+    {
+        public int radio=10;
+        public Circulo(string color, int poX, int poY):base(color, poX,poY)
+        {
+            color="negro";
+            poX=10;
+            poY=10;
+        }
+        public override void Dibuja()
+        {
+            Console.WriteLine("Se dibujó un circulo de color {0} en la posición ({1},{2})", color, poX,poY);
+        }
+    }
+    class Triangulo:Figura
+    {
+        public Triangulo(string color, int poX, int poY):base(color,poX,poY)
+        {
+            color="blanco";
+            poX=10;
+            poY=10;
+        }
+        public override void Dibuja()
+        {
+            Console.WriteLine("Se dibujó un triangulo de color {0} en la posición ({1},{2})", color, poX,poY);
+        }
+    }
+    class Cuadrado:Figura
+    {
+        public Cuadrado(string color, int poX, int poY):base(color,poX,poY)
+        {
+            color="blanco";
+            poX=10;
+            poY=10;
+        }
+        public override void Dibuja()
+        {
+            Console.WriteLine("Se dibujó un cuadrado de color {0} en la posición ({1},{2})", color, poX,poY);
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Figura> figuras=new List<Figura>();
+            Rectangulo a=new Rectangulo("rojo",60,100);
+            figuras.Add(a);
+            figuras.Add(new Circulo("azul",52,160));
+            figuras.Add(new Triangulo("verde",40,40));
+            figuras.Add(new Cuadrado("amarillo",81,90));
+            foreach (var f in figuras)
+            {
+                f.Dibuja();
+            }
         }
     }
 }

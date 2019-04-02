2fa7ae44d99ad43ce5f3839b34e5b20bdfcb9d32
Nyoseka <47579115+Nyoseka@users.noreply.github.com>
2019-04-02 15:13:26 -0800

    Update Program.cs

--- TareaUnidad4/Interfaces/Program.cs
+++ TareaUnidad4/Interfaces/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
 
 namespace Interfaces
-{
+{/*    abstract class Musico
+    {
+        public string nombre;
+        public Musico(string n)
+        {
+            nombre=n;
+        }
+        public abstract void Afina();
+        public virtual string Display()
+        {
+            return n;
+        }
+    }
+    class Bajista:Musico
+    {
+        public string instrumento;
+        public Bajista(string n, string i):base(nombre)
+        {
+            nombre=n;
+            instrumento=i;
+        }
+        public override void Afina()
+        {
+            Console.WriteLine("*afina bajo*");
+        }
+        public override string Display()
+        {
+            return n;
+        }
+    }
+    class Guitarrista:Musico
+    {
+        public string instrumento;
+        public Guitarrista(string nombre, string instrumento):base(nombre)
+        {
+            nombre=n;
+            instrumento=i;
+        }
+        public override void Afina()
+        {
+            Console.WriteLine("*afina guitarra*");
+        }
+        public override string Display()
+        {
+            return n;
+        }
+    }
+     */
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            List<Musico> musicos=new List<Musico>();
+            Bajista b = new Bajista("Flea");
+            Guitarrista g = new Guitarrista("Santana");
+            musicos.Add(b);
+            musicos.Add(g);
+            foreach (var m in musicos)
+            {
+                musicos.Display(m);
+            }
+             */
         }
     }
 }
